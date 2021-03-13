0d7f3c71a4aa5299059a1c19cd8793dccd43a556
unknown <deepjyoti120281@gmail.com>
2021-03-13 22:51:57 +0530

    form update with view table

--- resources/views/supervisor/channel_list.blade.php
+++ resources/views/supervisor/channel_list.blade.php
@@ -31,34 +31,35 @@ video')
     <th scope="col">View</th> --}}
 
 
-<table class="table table-bordered table-responsive">
-    <thead>
-        <tr>
-            <th scope="col-1" scope="col">ID</th>
-            <th scope="col-3" scope="col">Date</th>
-            <th scope="col-4" scope="col">School/College Name</th>
-            <th scope="col-2" scope="col">View</th>
-            <th class="col-2" scope="col">Action</th>
-        </tr>
-    </thead>
-    <tbody>
-        @foreach ($supervisorData as $item)
-        <tr>
-            <th scope="row">{{$item->id}}</th>
-            <td>{{$item->created_at->diffForHumans()}}</td>
-            <td>{{$item->channel->title}}</td>
-            <td>
-                <a class="btn btn-primary" href="{{url('supervisor/view/' . $item->id)}}">
-                    <img class="svgAction" src="/images/custom/icons/eye.svg" alt=""></a>
-            </td>
-            <td>
-                <a class="btn btn-success" href="{{url('supervisor/pdf/' . $item->id)}}">
-                    <img class="svgAction" src="/images/custom/icons/download.svg" alt=""></a>
-            </td>
-        </tr>
-        @endforeach
-    </tbody>
-</table>
+<div class="table-responsive">
+    <table class="table table-bordered table-hover table-condensed ">
+        <thead>
+            <tr>
+                <th scope="col">ID</th>
+                <th scope="col">Date</th>
+                <th scope="col">School/College Name</th>
+                <th class="text-center" scope="col">Action</th>
+                <!-- <th  scope="col">Download</th> -->
+            </tr>
+        </thead>
+        <tbody>
+            @foreach ($supervisorData as $item)
+            <tr>
+                <th scope="row">{{$item->id}}</th>
+                <td>{{$item->created_at->diffForHumans()}}</td>
+                <td>{{$item->channel->title}}</td>
+                <td class="text-center">
+                    <a class="btn btn-sm mt-1 btn-outline-primary" href="{{url('supervisor/view/' . $item->id)}}">
+                        <img class="svgAction" src="/images/custom/icons/eye.svg" alt=""></a>
+
+                    <a class="btn btn-sm mt-1 btn-outline-success" href="{{url('supervisor/pdf/' . $item->id)}}">
+                        <img class="svgAction" src="/images/custom/icons/download.svg" alt=""></a>
+                </td>
+            </tr>
+            @endforeach
+        </tbody>
+    </table>
+</div>
 
 
 
