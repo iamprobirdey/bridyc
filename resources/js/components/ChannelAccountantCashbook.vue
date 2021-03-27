205b303154ab252e00a204de67450368473617e5
unknown <deepjyoti120281@gmail.com>
2021-03-27 12:04:49 +0530

    accountant update design new new checkout

--- resources/js/components/ChannelAccountantCashbook.vue
+++ resources/js/components/ChannelAccountantCashbook.vue
@@ -64,7 +64,7 @@
                     style="cursor: pointer"
                     @click="editCashbook(cashbook, index)"
                   >
-                    Edited
+                    Edit
                     <svg
                       xmlns="http://www.w3.org/2000/svg"
                       viewBox="0 0 20 20"
